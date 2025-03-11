61e9634b776b16f875a3304971df44ada1533cee
Maharudra Yadav <88700864+maharudrayadav@users.noreply.github.com>
2025-03-11 23:52:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import json
 from flask import Flask, jsonify, request
 import subprocess
+import os
 
 app = Flask(__name__)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # Render assigns a dynamic port
+    app.run(host="0.0.0.0", port=port, debug=True)
 
 @app.route("/capture_faces", methods=["POST"])
 def capture_faces():
