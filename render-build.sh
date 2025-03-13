c17a44ad55e554c8fe80a63e96b2a94f306c3f1d
Maharudra Yadav <88700864+maharudrayadav@users.noreply.github.com>
2025-03-13 19:22:57 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+# Install system dependencies
+apt-get update && apt-get install -y \
+    build-essential \
+    cmake \
+    libopenblas-dev \
+    liblapack-dev \
+    libx11-dev \
+    libgtk2.0-dev \
+    libboost-all-dev
+
+# Upgrade pip and install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
