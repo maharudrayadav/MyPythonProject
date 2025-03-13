8522b4948d697fe94681a5ab6d2380bf43d5d301
Maharudra Yadav <88700864+maharudrayadav@users.noreply.github.com>
2025-03-13 19:06:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 from flask import Flask, request, jsonify
 from train_model import train_model
 from flask_cors import CORS
+from face_recognition import recognize_face
+
 
 
 # ✅ Load environment variables
