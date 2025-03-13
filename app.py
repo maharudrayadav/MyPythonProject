60539c62ddc0f9a4078adfc7409277b3a9aec15e
Maharudra Yadav <88700864+maharudrayadav@users.noreply.github.com>
2025-03-13 14:11:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import json
 import subprocess
 import sys
+import os
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 import capture_faces  # ✅ Import capture_faces.py directly
