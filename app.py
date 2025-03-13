b2338838dc3076632b2dab83ec52460a6d1d6566
Maharudra Yadav <88700864+maharudrayadav@users.noreply.github.com>
2025-03-14 01:01:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -123,3 +123,4 @@ def recognize():
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
     logging.info(f"🚀 Starting server on port {port}") 
+    app.run(host='0.0.0.0', port=5000)
